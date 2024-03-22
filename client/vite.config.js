f6c2810966d3e1135f480590c8c96087b8eb9369
Paul MarisOUMary <paul.marisoumary@gmail.com>
2024-03-22 18:43:07 +0100

    Working docker-compose embedded app

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://server:3001',
         changeOrigin: true,
         secure: false,
         ws: true,

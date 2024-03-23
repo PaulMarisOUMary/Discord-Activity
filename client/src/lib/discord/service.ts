99bc69f4301cf18b9d14e93772dbb5269cbd2bda
Paul MarisOUMary <paul.marisoumary@gmail.com>
2024-03-23 14:39:26 +0100

    fix typo tabulation

--- client/src/lib/discord/service.ts
+++ client/src/lib/discord/service.ts
@@ -16,35 +16,35 @@ class DiscordService {
     
         // Authorize with Discord Client
         const { code } = await this.sdk.commands.authorize({
-        client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
-        response_type: "code",
-        state: "",
-        prompt: "none",
-        scope: [
-            "identify",
-            "guilds",
-        ],
+            client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+            response_type: "code",
+            state: "",
+            prompt: "none",
+            scope: [
+                "identify",
+                "guilds",
+            ],
         });
     
         // Retrieve an access_token from your activity's server
         const response = await fetch("/api/token", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            code,
-        }),
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                code,
+            }),
         });
         const { access_token } = await response.json();
     
         // Authenticate with Discord client (using the access_token)
         this.auth = await this.sdk.commands.authenticate({
-        access_token,
+            access_token,
         });
     
         if (this.auth == null) {
-        throw new Error("Authenticate command failed");
+            throw new Error("Authenticate command failed");
         }
     }
 }
